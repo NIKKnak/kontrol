2f8e95452bea15b6b27a1cdc951f16d97c0302ee
Николай Ветошкин <nik88350759@yandex.ru>
2022-11-08 20:11:58 +0300

    3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+string[] array;
+WriteLine("Введите символы или значения через пробел");
+string stroka = ReadLine();
+array = stroka.Split(' ');
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+
+Write(string.Join(Environment.NewLine, result));
